b43af7c31db05c952c025e909f8912d9f223870f
JoshuaKerley <joshuakerley116@gmail.com>
2022-04-01 09:45:51 -0700

    Added state modification to list elements

--- src/Table.js
+++ src/Table.js
@@ -1,39 +1,46 @@
 import React from 'react'
 
-function TableHeader()  {
-    return (
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-    );
+function TableHeader()
+{
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Remove</th>
+      </tr>
+    </thead>
+  );
 }
 
-function TableBody(props) {
-    const rows = props.characterData.map((row, index) => {
-      return (
-        <tr key={index}>
-          <td>{row.name}</td>         
-          <td>{row.job}</td>
-        </tr>
-      );
-    });
+function TableBody(props)
+{
+  const rows = props.characterData.map((row, index) => {
     return (
-        <tbody>
-          {rows}
-        </tbody>
-     );
-  }
-
-function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
+      <tr key={index}>
+        <td>{row.name}</td>         
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
     );
+  });
+  return (
+    <tbody>
+      {rows}
+    </tbody>
+  );
+}
+
+function Table(props)
+{
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
 
 export default Table;
