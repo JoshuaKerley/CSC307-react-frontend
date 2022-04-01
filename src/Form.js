996fc855be2ac26341493da18780c9eb52c8772a
JoshuaKerley <joshuakerley116@gmail.com>
2022-04-01 12:14:56 -0700

    Implemented form submission

--- src/Form.js
+++ src/Form.js
@@ -1,26 +1,26 @@
 import React, {useState} from 'react';
 
-function Form()
+function Form(props)
 {
-  const [person, setPerson] = useState(
-    {
-        name: "",
-        job: "",
-    }
-  );
+    const [person, setPerson] = useState(
+        {
+            name: "",
+            job: "",
+        }
+    );
 
-  function handleChange(event)
-  {
-    const { name, value } = event.target;
-    if (name === "job")
-      setPerson(
-         {name: person['name'], job: value}
-      );
-    else     
-       setPerson(
-         {name: value, job: person['job']}   
-       );
-  }
+    function handleChange(event)
+    {
+        const { name, value } = event.target;
+        if (name === "job")
+        setPerson(
+            {name: person['name'], job: value}
+        );
+        else     
+        setPerson(
+            {name: value, job: person['job']}   
+        );
+    }
 
     return (
         <form>
@@ -38,8 +38,15 @@ function Form()
             id="job"
             value={person.job}
             onChange={handleChange} />
+        <input type="button" value="Submit" onClick={submitForm} />
         </form>
     );
+
+    function submitForm()
+    {
+        props.handleSubmit(person);
+        setPerson({name: '', job: ''});
+    }
 }
 
 export default Form;
--- src/MyApp.js
+++ src/MyApp.js
@@ -9,7 +9,7 @@ function MyApp()
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <Form />
+      <Form handleSubmit={updateList} />
     </div>
   )
 
@@ -19,6 +19,10 @@ function MyApp()
       return i !== index
     });
     setCharacters(updated);
+  }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
   }
 }
 
