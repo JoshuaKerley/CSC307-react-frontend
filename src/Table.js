119cdfecc548d4d0e7d2272e7011f9631aa88506
JoshuaKerley <joshuakerley116@gmail.com>
2022-04-06 13:58:15 -0700

    [5] added ID column to table

--- src/Table.js
+++ src/Table.js
@@ -7,6 +7,7 @@ function TableHeader()
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>ID</th>
         <th>Remove</th>
       </tr>
     </thead>
@@ -18,8 +19,9 @@ function TableBody(props)
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{row.name}</td>         
+        <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>{row.id}</td>
         <td>
           <button onClick={() => props.removeCharacter(index)}>Delete</button>
         </td>
